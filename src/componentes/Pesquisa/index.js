8e880d02b00e6b22ac92881271fb8eb490d06853
Goethe Carvalho <goethecarvalho@gmail.com>
2024-03-22 09:13:56 -0300

    Componentes dinâmicos

--- src/componentes/Pesquisa/index.js
+++ src/componentes/Pesquisa/index.js
@@ -25,6 +25,23 @@ const Subtitulo = styled.h3`
     margin-bottom: 40px;
 `
 
+const Resultado = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin-bottom: 20px;
+    cursor: pointer;
+    p {
+        width: 200px;
+    }
+    img {
+        width: 100px;
+    }
+    &:hover {
+        border: 1px solid white;
+    }
+`
+
 function Pesquisa() {
     const [ livrosPesquisados, setLivrosPesquisados ] = useState([])
 
@@ -42,6 +59,12 @@ function Pesquisa() {
                     setLivrosPesquisados(resultadoPesquisa)
                 }}
             />
+            { livrosPesquisados.map( livro => (
+                <Resultado>
+                    <img src={livro.src}/>
+                    <p>{livro.nome}</p>
+                </Resultado>
+            )) }
         </PesquisaContainer>
     )
 }
