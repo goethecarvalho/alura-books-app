25d318342d29d91056416383f4108dbeef2339a8
Goethe Carvalho <goethecarvalho@gmail.com>
2024-03-22 08:51:27 -0300

    Styled Component - React useState

--- src/componentes/Pesquisa/index.js
+++ src/componentes/Pesquisa/index.js
@@ -1,5 +1,6 @@
 import Input from '../Input'
 import styled from 'styled-components'
+import { useState } from 'react'
 
 const PesquisaContainer = styled.section`
     background-image: linear-gradient(90deg, #002F52 35%, #326589 165%);
@@ -24,13 +25,17 @@ const Subtitulo = styled.h3`
 `
 
 function Pesquisa() {
+    const [ textoDigitado, setTextoDigitado ] = useState('')
+    
     return (
         <PesquisaContainer>
             <Titulo>Já sabe por onde começar?</Titulo>
             <Subtitulo>Encontre seu livro em nossa estante.</Subtitulo>
             <Input
                 placeholder="Escreva sua próxima leitura"
+                onBlur={evento => setTextoDigitado(evento.target.value)}
             />
+            <p>{textoDigitado}</p>
         </PesquisaContainer>
     )
 }
