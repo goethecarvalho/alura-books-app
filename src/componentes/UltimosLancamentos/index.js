de099ac244a082d125aa0ad62b85383b817d22bb
Goethe Carvalho <goethecarvalho@gmail.com>
2024-03-22 10:04:35 -0300

    Props - estilizando

--- src/componentes/UltimosLancamentos/index.js
+++ src/componentes/UltimosLancamentos/index.js
@@ -1,4 +1,5 @@
 import { livros } from './dadosUltimosLancamentos'
+import { Titulo }  from '../Titulo'
 import styled from 'styled-components'
 
 const UltimosLancamentosContainer = styled.section`
@@ -6,16 +7,6 @@ const UltimosLancamentosContainer = styled.section`
     padding-bottom: 20px;
     display: flex;
     flex-direction: column;
-`
-
-const Titulo = styled.h2`
-    width: 100%;
-    padding: 30px 0;
-    background-color: #FFF;
-    color: #EB9B00;
-    font-size: 36px;
-    text-align: center;
-    margin: 0;
 `
 
 const NovosLivrosContainer = styled.div`
@@ -29,7 +20,12 @@ const NovosLivrosContainer = styled.div`
 function UltimosLancamentos() {
     return (
         <UltimosLancamentosContainer>
-            <Titulo>ULTIMOS LANCAMENTOS</Titulo>
+            <Titulo
+                cor={"#EB9B00"}
+                tamanhoFonte={"36px"}
+            >
+                ÚLTIMOS LANÇAMENTOS
+            </Titulo>
             <NovosLivrosContainer>
                 { livros.map( livro => (
                     <img src={livro.src}/>
