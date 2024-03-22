57cdbc6a38b01665589d309d213cf61a6fadf15a
Goethe Carvalho <goethecarvalho@gmail.com>
2024-03-22 08:04:04 -0300

    Styled Components - Trocando arquivos CSS

--- src/componentes/Logo/index.js
+++ src/componentes/Logo/index.js
@@ -1,16 +1,23 @@
-import './estilo.css'
 import logo from '../../imagens/logo.svg'
+import styled from 'styled-components'
 
-function Logo() {
-    return (
-        <div className='logo'>
-            <img
+const LogoContainer = styled.div`
+    display: flex;
+    font-size: 30px;
+`
+const LogoImage = styled.img`
+    margin-right: 10px;
+`
+
+function Logo () {
+    return(
+        <LogoContainer>
+            <LogoImage
                 src={logo}
-                alt='logo' 
-                className='logo-img'
-            ></img>
+                alt='logo'
+            />
             <p><strong>Alura</strong>Books</p>
-       </div>
+        </LogoContainer>
     )
 }
 
